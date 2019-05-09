3f422697f62c86dbf1094dd94960dcbae04fb33b
lingw <lingw@akulaku.com>
2019-05-09 11:38:42 +0800

    通过cross-env 设置Node环境变量, 并且在webpack设置对应环境的mode

--- README.md
+++ README.md
@@ -16,7 +16,7 @@
 - uglify-es
 - opn
 - connect-history-api-fallback
-- cross-env
+- cross-env：屏蔽开发所处操作系统的差异，提供一致的设置环境变量的命令。
 - lint-staged
 
 ## loader
--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,6 +1,9 @@
 const path = require('path');
 
+const env = process.env.NODE_ENV;
+
 module.exports = {
+  mode: env === 'production' ? 'production' : 'development',
   entry: {
     app: './src/main.js',
   },

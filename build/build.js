802c7256904c14cd44b4c552084c1a7c46b9f480
lingw <lingw@akulaku.com>
2019-05-10 15:43:46 +0800

    rimraf删除旧的构建文件

---
+++ build/build.js
@@ -0,0 +1,31 @@
+const webpack = require('webpack');
+const rm = require('rimraf');
+const path = require('path');
+const prodConfig = require('./webpack.prod.conf');
+const config = require('../config');
+
+rm(
+  path.join(config.build.assetsRoot, config.build.assetsSubDirectory),
+  (err) => {
+    if (err) throw err;
+    webpack(prodConfig, (err, stats) => {
+      if (err) throw err;
+      process.stdout.write(
+        stats.toString({
+          colors: true,
+          modules: false,
+          children: false, // If you are using ts-loader, setting this to true will make TypeScript errors show up during build.
+          chunks: false,
+          chunkModules: false,
+        }) + '\n\n',
+      );
+    });
+  },
+);
+
+/* rm(
+  path.join(config.build.assetsRoot, config.build.assetsSubDirectory),
+  (err) => {
+    if (err) throw err;
+  },
+); */
